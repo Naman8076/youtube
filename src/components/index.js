d55b0eda8ab35e021066d5394d204bf78b430ab4
naman <namansinghal2110@gmail.com>
2023-05-15 20:31:09 +0530

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+export {default as Navbar} from './Navbar'//5
+export {default as Feed} from './Feed'//4
+export {default as ChannelDetail} from './ChannelDetail'//3
+export {default as SearchFeed} from './SearchFeed'//9
+export {default as VideoDetail} from './VideoDetail'//8
+export {default as SearchBar} from './SearchBar';//10
+export {default as Sidebar} from './Sidebar';//7
+export {default as Videos} from './Videos';//9
+export {default as VideoCard} from './VideoCard';//2
+export {default as ChannelCard} from './ChannelCard';//1
+export {default as Loader} from './Loader'//6
+
